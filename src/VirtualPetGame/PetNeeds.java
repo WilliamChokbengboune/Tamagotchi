c1ab33011f70b9c69237a56f469e520dde20115f
willc <wchokbengboune@ggc.edu>
2023-04-20 20:04:39 -0400

    VirtualPetSimulator.java is created and closed

--- src/VirtualPetGame/PetNeeds.java
+++ src/VirtualPetGame/PetNeeds.java
@@ -11,4 +11,69 @@ public class PetNeeds {
         happiness = 50;
     }
 
+    public void decreaseHunger(int amount){
+        hunger -= amount;
+        if (hunger < 0){
+            hunger = 0;
+        }
+    }
+
+    public void increaseHunger(int amount){
+        hunger += amount;
+        if (hunger > 100){
+            hunger = 100;
+        }
+    }
+
+    public void decreaseThirst(int amount){
+        thirst -= amount;
+        if (thirst < 0){
+            thirst = 0;
+        }
+    }
+
+    public void increaseThirst(int amount){
+        thirst += amount;
+        if (thirst > 100){
+            thirst = 100;
+        }
+    }
+
+    public void decreaseHappiness(int amount){
+        happiness -= amount;
+        if (happiness < 0){
+            happiness = 0;
+        }
+    }
+
+    public void increaseHappiness(int amount){
+        happiness += amount;
+        if (happiness > 100){
+            happiness = 100;
+        }
+    }
+
+    public int getHappiness() {
+        return happiness;
+    }
+
+    public int getThirst() {
+        return thirst;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public void setHappiness(int happiness) {
+        this.happiness = happiness;
+    }
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public void setThirst(int thirst) {
+        this.thirst = thirst;
+    }
 }
